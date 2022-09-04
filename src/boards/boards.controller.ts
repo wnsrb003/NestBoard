c765d2e00eb0d2890e5ee16d3adcb51f2cc91df5
박준규 <wnsrb003@naver.com>
2022-09-04 18:25:54 +0900

    CRUD Controller, Service Add

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { title } from 'process';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service'
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -13,12 +13,28 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board{
+        return this.boardsService.getBoardById(id);
+    }
+
     @Post('/')
     createBoard(
         @Body() createBoardDto: CreateBoardDto) {
         return this.boardsService.createBoard(createBoardDto);
     }
     
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void{
+        return this.boardsService.deleteBoard(id)
+    }
+
+    @Patch('/:id')
+    updateBoardStatus(
+        @Param('id') id: string, 
+        @Body('status') status: BoardStatus): Board{
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 }
 // export class BoardsController {
 //     boardsService: BoardsService;
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import {v1 as uuid} from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { abort } from 'process';
 
 @Injectable()
 export class BoardsService {
@@ -9,6 +10,10 @@ export class BoardsService {
 
     getAllBoards(): Board[]{
         return this.boards;
+    }
+
+    getBoardById(id: string): Board{
+        return this.boards.find((aBoard) => aBoard.id === id)
     }
 
     createBoard(createBoardDto: CreateBoardDto): Board{
@@ -23,5 +28,15 @@ export class BoardsService {
 
         this.boards.push(newBoard);
         return newBoard;
+    }
+
+    deleteBoard(id: string): void{
+        this.boards = this.boards.filter((aBoard) => aBoard.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board{
+        const aBoard = this.getBoardById(id);
+        aBoard.status = status;
+        return aBoard;
     }
 }
